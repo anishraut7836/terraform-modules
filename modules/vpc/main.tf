3dd31beaff3e8f94bd8123460c83c42b539d07fb
Anish Kumar <anishraut7836@gmail.com>
2024-10-12 12:33:41 +0530

    updated vpc module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,5 +1,33 @@
+locals {
+  len_public_subnets      = max(length(var.public_subnets))#, length(var.public_subnet_ipv6_prefixes))
+  len_private_subnets     = max(length(var.private_subnets))#, length(var.private_subnet_ipv6_prefixes))
+ # len_database_subnets    = max(length(var.database_subnets), length(var.database_subnet_ipv6_prefixes))
+ # len_elasticache_subnets = max(length(var.elasticache_subnets), length(var.elasticache_subnet_ipv6_prefixes))
+ # len_redshift_subnets    = max(length(var.redshift_subnets), length(var.redshift_subnet_ipv6_prefixes))
+ # len_intra_subnets       = max(length(var.intra_subnets), length(var.intra_subnet_ipv6_prefixes))
+ # len_outpost_subnets     = max(length(var.outpost_subnets), length(var.outpost_subnet_ipv6_prefixes))
+
+  max_subnet_length = max(
+    local.len_private_subnets,
+    local.len_public_subnets,
+   # local.len_elasticache_subnets,
+   # local.len_database_subnets,
+   # local.len_redshift_subnets,
+  )
+
+  # Use `local.vpc_id` to give a hint to Terraform that subnets should be deleted before secondary CIDR blocks can be free!
+  vpc_id = try(aws_vpc_ipv4_cidr_block_association.this[0].vpc_id, aws_vpc.this[0].id, "")
+
+  create_vpc = var.create_vpc && var.putin_khuylo
+}
+
+
 resource "aws_vpc" "firs_vpc" {
-  cidr_block = var.cidr
+  count = local.create_vpc ? 1 : 0
+
+  cidr_block          = var.use_ipam_pool ? null : var.cidr
+  ipv4_ipam_pool_id   = var.ipv4_ipam_pool_id
+  ipv4_netmask_length = var.ipv4_netmask_length
   instance_tenancy = var.instance_tenancy
   enable_dns_hostnames     = var.enable_dns_hostnames
   enable_dns_support       = var.enable_dns_support
@@ -10,6 +38,16 @@ resource "aws_vpc" "firs_vpc" {
     var.vpc_tags,
   )
 }
+
+resource "aws_vpc_ipv4_cidr_block_association" "this" {
+  count = local.create_vpc && length(var.secondary_cidr_blocks) > 0 ? length(var.secondary_cidr_blocks) : 0
+
+  # Do not turn this into `local.vpc_id`
+  vpc_id = aws_vpc.this[0].id
+
+  cidr_block = element(var.secondary_cidr_blocks, count.index)
+}
+
 
 resource "aws_subnet" "public" {
   availability_zone = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,40 @@
+variable "create_vpc" {
+  description = "Controls if VPC should be created (it affects almost all resources)"
+  type        = bool
+  default     = true
+}
+
+variable "putin_khuylo" {
+  description = "Do you agree that Putin doesn't respect Ukrainian sovereignty and territorial integrity? More info: https://en.wikipedia.org/wiki/Putin_khuylo!"
+  type        = bool
+  default     = true
+}
+
+variable "use_ipam_pool" {
+  description = "Determines whether IPAM pool is used for CIDR allocation"
+  type        = bool
+  default     = false
+}
+
+variable "ipv4_ipam_pool_id" {
+  description = "(Optional) The ID of an IPv4 IPAM pool you want to use for allocating this VPC's CIDR"
+  type        = string
+  default     = null
+}
+
+variable "ipv4_netmask_length" {
+  description = "(Optional) The netmask length of the IPv4 CIDR you want to allocate to this VPC. Requires specifying a ipv4_ipam_pool_id"
+  type        = number
+  default     = null
+}
+
+variable "secondary_cidr_blocks" {
+  description = "List of secondary CIDR blocks to associate with the VPC to extend the IP Address pool"
+  type        = list(string)
+  default     = []
+}
+
+
 variable "name" {
   description = "Name to be used on all the resources as identifier"
   type        = string
