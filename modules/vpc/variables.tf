bd8684bbf5911b8202e1c6661b55251b4997ca3d
Anish Kumar <anishraut7836@gmail.com>
2024-10-12 22:00:19 +0530

    deuplicate variable removed

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -85,11 +85,6 @@ variable "azs" {
   default     = []
 }
 
-variable "one_nat_gateway_per_az" {
-  description = "Should be true if you want only one NAT Gateway per availability zone. Requires `var.azs` to be set, and the number of `public_subnets` created to be greater than or equal to the number of availability zones specified in `var.azs`"
-  type        = bool
-  default     = false
-}
 
 variable "public_subnets" {
   description = "A list of public subnets inside the VPC"
